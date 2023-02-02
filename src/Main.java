d89f038d571ae587cec29c630bc0935780aab0b3
Muhoboyka <mamiha72@mail.ru>
2023-02-02 17:28:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 990;
+        int bonusCost = 20;
+        int bonusMiles = ticketCost / bonusCost;
+
+        System.out.println(bonusMiles);
+    }
+}
